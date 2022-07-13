b5f94397e8fb33bcf82fa92d0a88a5f87b4eb7da
Hashastatus <109164074+Hashastatus@users.noreply.github.com>
2022-07-13 15:12:28 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['+94715985737']
+global.premium = ['+94715985737']
+global.ownernomer = '+94715985737'
+global.ownername = '🙋‍♂️ 𝐁𝐨𝐭 𝐇𝐚𝐬𝐡𝐚'
+global.botname = '𝐁𝐨𝐭-𝐇𝐚𝐬𝐡𝐚'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
